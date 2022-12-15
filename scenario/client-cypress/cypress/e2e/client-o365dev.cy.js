e643094eca2a2f41396bea1b798fa561ff846eba
Lars Kåre Skjørestad <larskaare@users.noreply.github.com>
2022-12-15 18:15:03 +0100

    Update Cypress Tests

--- scenario/client-cypress/cypress/e2e/client-o365dev.cy.js
+++ scenario/client-cypress/cypress/e2e/client-o365dev.cy.js
@@ -1,182 +1,136 @@
-//Client Tests
 
-const login = (name) => {
-    cy.session(name, () => {
-        cy.request({
-            method: 'GET',
-            url: '/login',
-        }).then(() => {
-            // nothing here yet//
-        });
-    });
-};
+function logIn() {
+    cy.visit('/');
+    cy.get('#SignIn').click();
+}
 
 describe('Client should open and render initial page', () => {
 
-    it('Open client', () => {
+    it('Opens initial web page', () => {
 
-        cy.session('test-user', () => {
+        cy.visit('/');
+        cy.contains('Game of Thrones Episodes');
+        cy.get('#SignIn').should('contain', 'Sign In');
+        cy.getCookies('sessionID').should('exist');
+        
+    });
+ 
+    it('Should be able to sign in', () => {
+        cy.visit('/');
+        cy.get('#SignIn').click();
 
-            cy.visit('/');
-            cy.contains('Game of Thrones Episodes');
-            cy.get('#SignIn').should('contain', 'Sign In');
-            cy.getCookies('sessionID').should('exist');
-    
-        });
+        cy.contains('Game of Thrones Episodes');
+        cy.get('#Logout').should('contain', 'Logout');  
+    });
+
+});
+
+describe('Main menu should render and we should be able to log-out', () => {
+
+    it('We sign in', () => {
+        logIn();
+
+        cy.contains('Game of Thrones Episodes');
+        cy.get('#Logout').should('contain', 'Logout');
+    });
+
+   
+    it('Main meny contains correct items', () => {
+        logIn();
+
+        cy.get('#Logout').should('contain', 'Logout');
+        cy.get('#showinbox').should('contain', 'Show Inbox');        
+        cy.get('#got').should('contain','Show GOT episodes');
+        cy.get('#showindex').should('contain','Home');
+    });
+
+    it('We do a log-out', () => {
+        logIn();
+
+        cy.get('#Logout').click();
+
+        cy.contains('Game of Thrones Episodes');
+        cy.get('#SignIn').should('contain', 'Sign In');
 
     });
 
 });
 
-// describe('Login should show initial screen', () => {
+describe('The inbox should be accessible', () => {
 
-//     // before(() => {
-//     //     // cy.clearCookies();
-//     //     cy.session('test', () => {
-//     //         cy.visit('/');
-//     //         cy.get('#SignIn').click();
+    beforeEach(() => {
+        logIn();
+    });
 
-//     //     });
-//     //     // cy.visit('/');
-//     //     // cy.get('#SignIn').click();
-//     // });
-
-//     beforeEach(() => {
-//         // Cypress.Cookies.preserveOnce('sessionID');
-//     });
- 
-//     it('It should be able to signin - to the Office365 Dev Test Tenant', () => {
-
-//         cy.session('test', () => {
-
-//             cy.visit('/');
-//             cy.get('#SignIn').click();
-
-//             cy.contains('Game of Thrones Episodes');
-//             cy.get('#Logout').should('contain', 'Logout');
-//         });
-//     });
-
-    // it('The initial screen should contain the basic menu', () => {
-
-    //     cy.get('#Logout').should('contain', 'Logout');
-    //     cy.get('#showinbox').should('contain', 'Show Inbox');        
-    //     cy.get('#got').should('contain','Show GOT episodes');
-    //     cy.get('#showindex').should('contain','Home');
-    // });
-
-    // it('It should be able to log out', () => {
-    //     cy.get('#Logout').click();
-
-    //     cy.contains('Game of Thrones Episodes');
-    //     cy.get('#SignIn').should('contain', 'Sign In');
-
-    // });
-
-// });
-
-// describe('The inbox should be shown', () => {
-
-//     before(() => {
-//         cy.clearCookies();
-//         cy.visit('/');
-//         cy.get('#SignIn').click();
-//     });
-
-//     beforeEach(() => {
-//         // Cypress.Cookies.preserveOnce('sessionID');
-//     });
-
-//     it('It should show inbox',  () => {
+    it('We can select the inbox',  () => {
    
-//         cy.get('#showinbox').click();
+        cy.get('#showinbox').click();
        
-//     });
+    });
 
-//     it('It should be the inbox of the test user Adele', () => {
+    it('The Inbox shows data for test user Adele', () => {
 
-//         cy.fixture('test-user','utf-8').then((testUser) => {
+        cy.fixture('test-user','utf-8').then((testUser) => {
         
-//             cy.contains('List my inbox');
-//             cy.contains(testUser.name);
-//             cy.contains(testUser.email); 
+            cy.contains('List my inbox');
+            cy.contains(testUser.name);
+            cy.contains(testUser.email); 
           
-//         });        
-//     });
+        });        
+    });
 
-//     it('It should be able to log out', () => {
-//         cy.get('#Logout').click();
+});
 
-//         cy.contains('Game of Thrones Episodes');
-//         cy.get('#SignIn').should('contain', 'Sign In');
-//     });
+describe('We should get a list of GOT Episodes', () => {
 
-// });
+    beforeEach(() => {
+        logIn();
+    });
 
-// describe('We should get a list of GOT Episodes', () => {
-
-//     before(() => {
-//         cy.clearCookies();
-//         cy.visit('/');
-//         cy.get('#SignIn').click();
-//     });
-
-//     beforeEach(() => {
-//         // Cypress.Cookies.preserveOnce('sessionID');
-//     });
-
-//     it('We should be able to log in and select got episodes', () => {
-//         cy.get('#got').should('contain', 'Show GOT episodes');
-//         cy.get('#Logout').should('contain', 'Logout');
-//         cy.get('#got').click();
-//     });
+    it('We should be able to log in and select got episodes', () => {
+        cy.get('#got').should('contain', 'Show GOT episodes');
+        cy.get('#Logout').should('contain', 'Logout');
+        cy.get('#got').click();
+    });
 
 
-//     it('It should request GOT episodes', () => {
-//         cy.contains('Got Episodes');
-//         cy.contains('Winter is coming');
-//         cy.contains('Quote:');
-//     });
+    it('We should request GOT episodes', () => {
+        cy.get('#got').click();
+        cy.contains('Got Episodes');
+        cy.contains('Winter is coming');
+        cy.contains('Quote:');
+    });
 
-//     it('It should be able to go home', () => {
-//         cy.get('#showindex').click();
-//         cy.contains('List my inbox');
-//         cy.get('#Logout').should('contain', 'Logout');
-//     });
+    it('We should be able to select HOME', () => {
+        cy.get('#showindex').click();
+        cy.contains('List my inbox');
+        cy.get('#Logout').should('contain', 'Logout');
+    });
 
-//     it('It should request GOT episodes directly', () => {
-//     //    cy.visit('http://localhost:3000/login');
-//         cy.visit('/got');
-//         cy.contains('Got Episodes');
-//         cy.contains('Winter is coming');
-//         cy.contains('Quote:');
-//     });
+    it('We should request GOT episodes directly', () => {
+        cy.visit('/got');
+        cy.contains('Got Episodes');
+        cy.contains('Winter is coming');
+        cy.contains('Quote:');
+    });
 
-//     it('It should be able to log out', () => {
-//         cy.get('#Logout').click();
+});
 
-//         cy.contains('Game of Thrones Episodes');
-//         cy.get('#SignIn').should('contain', 'Sign In');
-//     });
+describe('Client should serve proper headers', () => {
 
-// });
-
-
-// describe('Client should serve proper headers', () => {
-
-//     it('Response header contains proper CSP', () => {
-//         cy.request('/').should((response) => {
+    it('Response header contains proper CSP', () => {
+        cy.request('/').should((response) => {
             
            
-//             console.log(response.headers);
+            console.log(response.headers);
 
-//             expect(response).to.have.property('headers');
-//             expect(response.headers).to.have.property('content-security-policy');
+            expect(response).to.have.property('headers');
+            expect(response.headers).to.have.property('content-security-policy');
 
-//             expect(response.headers['content-security-policy']).to.have.string('default-src \'self\'');
-//             expect(response.headers['content-security-policy']).to.have.string('block-all-mixed-content');
-//             expect(response.headers['content-security-policy']).to.have.string('script-src \'self\'');
-//         });
-//     });
+            expect(response.headers['content-security-policy']).to.have.string('default-src \'self\'');
+            expect(response.headers['content-security-policy']).to.have.string('block-all-mixed-content');
+            expect(response.headers['content-security-policy']).to.have.string('script-src \'self\'');
+        });
+    });
         
-// });
+});
